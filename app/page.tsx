0fd64dea6d13b45cc5d441e81abfb6ecdf716e0c
Paul <paulruales1@gmail.com>
2023-04-23 20:32:42 -0400

    update intro

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Hi, my name is Paul, I'm a software engineer and builder. <br /> I
+          Hi, my name is Paul, I'm a software engineer and builder. <br /> I've
           shipped features to millions of customers at Amazon <br /> and I'm
           currently freelancing and building my own products.
         </h2>
